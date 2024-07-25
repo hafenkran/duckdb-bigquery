80db23b77bb21a7896b9343c3f5b8b64a03d54c2
hafenkran <4216598+hafenkran@users.noreply.github.com>
2024-07-25 21:34:53 +0200

    added various alter table info to sql

--- src/include/bigquery_sql.hpp
+++ src/include/bigquery_sql.hpp
@@ -20,6 +20,7 @@ public:
     static string TransformFilter(const string &column_name, TableFilter &filter);
     static string CreateExpression(const string &column_name, vector<unique_ptr<TableFilter>> &filters, const string &op);
 
+	static string AlterTableInfoToSQL(const string &project_id, const AlterTableInfo &info);
     static string CreateSchemaInfoToSQL(const string &project_id, const CreateSchemaInfo &info);
     static string CreateTableInfoToSQL(const string &project_id, const CreateTableInfo &info);
     static string CreateViewInfoToSQL(const string &project_id, const CreateViewInfo &info);
@@ -28,6 +29,7 @@ public:
     static string LogicalUpdateToSQL(const string &project_id, LogicalUpdate &lu, PhysicalOperator &child);
     static string LogicalDeleteToSQL(const string &project_id, LogicalDelete &ld, PhysicalOperator &child);
 
+	static string BigqueryColumnToSQL(const ColumnDefinition &column);
     static string BigqueryColumnsToSQL(const ColumnList &columns, const vector<unique_ptr<Constraint>> &constraints);
 };
 
