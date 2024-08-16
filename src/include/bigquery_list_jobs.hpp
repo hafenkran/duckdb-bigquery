c700d0c947b617bab22d0000f36835b0606ee0d7
hafenkran <4216598+hafenkran@users.noreply.github.com>
2024-08-16 21:35:23 +0200

    added list_jobs func header

---
+++ src/include/bigquery_list_jobs.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "duckdb.hpp"
+
+namespace duckdb {
+namespace bigquery {
+
+class BigQueryListJobsFunction : public TableFunction {
+public:
+    BigQueryListJobsFunction();
+};
+
+} // namespace bigquery
+} // namespace duckdb
