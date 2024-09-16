cb7727c15e130b3a396dd43a372aca616470ec1b
hafenkran <4216598+hafenkran@users.noreply.github.com>
2024-09-16 23:45:40 +0200

    added jobs infos to bigquery execute results

--- src/bigquery_execute.cpp
+++ src/bigquery_execute.cpp
@@ -28,7 +28,19 @@ static duckdb::unique_ptr<FunctionData> BigQueryExecuteBind(ClientContext &conte
                                                             vector<LogicalType> &return_types,
                                                             vector<string> &names) {
     return_types.emplace_back(LogicalTypeId::BOOLEAN);
-    names.emplace_back("Success");
+    names.emplace_back("success");
+	return_types.emplace_back(LogicalTypeId::VARCHAR);
+	names.emplace_back("job_id");
+	return_types.emplace_back(LogicalTypeId::VARCHAR);
+	names.emplace_back("project_id");
+	return_types.emplace_back(LogicalTypeId::VARCHAR);
+	names.emplace_back("location");
+	return_types.emplace_back(LogicalTypeId::UBIGINT);
+	names.emplace_back("total_rows");
+	return_types.emplace_back(LogicalTypeId::BIGINT);
+	names.emplace_back("total_bytes_processed");
+	return_types.emplace_back(LogicalTypeId::VARCHAR);
+	names.emplace_back("num_dml_affected_rows");
 
     auto database_name = input.inputs[0].GetValue<string>();
     auto &database_manager = DatabaseManager::Get(context);
@@ -58,10 +70,17 @@ static void BigQueryExecuteFunc(ClientContext &context, TableFunctionInput &data
         throw BinderException("Cannot execute BigQuery query in read-only transaction");
     }
     auto bqclient = transaction.GetBigqueryClient();
-    bqclient->ExecuteQuery(data.query);
+    auto response = bqclient->ExecuteQuery(data.query);
 
-    data.finished = true;
-    output.data[0].SetValue(0, true);
+	data.finished = true;
+	output.SetValue(0, 0, true);
+	output.SetValue(1, 0, response.job_reference().job_id());
+	output.SetValue(2, 0, response.job_reference().project_id());
+	output.SetValue(3, 0, response.job_reference().location().value());
+	output.SetValue(4, 0, Value::UBIGINT(response.total_rows().value()));
+	output.SetValue(5, 0, Value::BIGINT(response.total_bytes_processed().value()));
+	output.SetValue(6, 0, Value::BIGINT(response.num_dml_affected_rows().value()));
+	output.SetCardinality(1);
 }
 
 BigQueryExecuteFunction::BigQueryExecuteFunction()
