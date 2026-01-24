9dc667a158fb276464c155309f59b3b57a42168f
hafenkran <4216598+hafenkran@users.noreply.github.com>
2026-01-24 16:46:29 +0100

    added bigquery_table_storage func

--- src/include/bigquery_query.hpp
+++ src/include/bigquery_query.hpp
@@ -15,6 +15,26 @@ struct BigqueryQueryDryRunBindData : public TableFunctionData {
     bool finished = false;
 };
 
+unique_ptr<FunctionData> BigqueryQueryBindInternal(ClientContext &context,
+                                                   const string &dbname_or_project_id,
+                                                   const string &query_string,
+                                                   const BigQueryCommonParameters &params,
+                                                   vector<LogicalType> &return_types,
+                                                   vector<string> &names,
+                                                   bool allow_dry_run);
+
+unique_ptr<GlobalTableFunctionState> BigqueryQueryInitGlobal(ClientContext &context, TableFunctionInitInput &input);
+unique_ptr<LocalTableFunctionState> BigqueryQueryInitLocal(ExecutionContext &context,
+                                                           TableFunctionInitInput &input,
+                                                           GlobalTableFunctionState *global_state);
+void BigqueryQueryExecute(ClientContext &context, TableFunctionInput &data, DataChunk &output);
+BindInfo BigqueryQueryGetBindInfo(const optional_ptr<FunctionData> bind_data_p);
+InsertionOrderPreservingMap<string> BigqueryQueryToString(TableFunctionToStringInput &input);
+unique_ptr<NodeStatistics> BigqueryQueryCardinality(ClientContext &context, const FunctionData *bind_data_p);
+double BigqueryQueryProgress(ClientContext &context,
+                             const FunctionData *bind_data_p,
+                             const GlobalTableFunctionState *global_state);
+
 class BigqueryQueryFunction : public TableFunction {
 public:
     BigqueryQueryFunction();
